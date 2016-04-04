65dcfd23f06f61ce0265e1a29be1f454923e32b8
peponi <mail@peponi.tk>
2016-04-04 08:53:48 +0200

    replace fetch by pouchdb
    code needs a firefox os > 1.2 bugfix (stackoverflow link in comment)
    
    code needs to be organized

--- README.md
+++ README.md
@@ -17,15 +17,31 @@
   * check if contact already exist on FirefoxOS - by comparing phone or email or name
   * v1.2
   * v1.3
+
+### FirefoxOS API
+
+* https://developer.mozilla.org/en-US/docs/Mozilla/Firefox_OS/API/Contacts_API
+* https://developer.mozilla.org/en-US/Apps/Fundamentals/gather_and_modify_data/Updating_phone_contacts_from_the_web
  
 ### Firmware upgrade links:
 
-1.  **FFOS1.3** image for *ZTE Open* & *Alcatel One Touch Fire* on [firefoxosbuilds.org](http://firefoxosbuilds.org) 
-2. [upgrade to](https://hacks.mozilla.org/2014/01/upgrading-your-zte-open-to-firefox-1-1-or-1-2-fastboot-enabled/) **FFOS1.2**
-3. [upgrade to](http://opendirective.net/blog/2014/04/success-firefox-os-1-4-built-and-running-on-zte-open/) **FFOS1.4/FFOS1.5**
-4. upgrade/reset to FFOS1.1/FFOS1.0  ([firmware for ZTE open](http://www.ztedevice.com/support/smart_phone/b5a2981a-1714-4ac7-89e1-630e93e220f8.html) )
+* all you need for for *ZTE Open*, *Alcatel One Touch Fire*, *Keon* & *Flame* on [www.mexmod.com](http://www.mexmod.com)
+* all you need for Geekphones on [downloads.geeksphone.com](http://downloads.geeksphone.com)
+* all you need for Sony FXP on [fxpblog.co](http://fxpblog.co/firefox/nightly)
+*  see a huge list of build for several devices up to FFOS 2.0 on [firefoxosbuilds.org](https://firefoxos.mozilla.community/devices) 
+* [upgrade to](https://hacks.mozilla.org/2014/01/upgrading-your-zte-open-to-firefox-1-1-or-1-2-fastboot-enabled/) **FFOS1.2**
+* [upgrade to](http://opendirective.net/blog/2014/04/success-firefox-os-1-4-built-and-running-on-zte-open/) **FFOS1.4/FFOS1.5**
+* upgrade/reset to FFOS1.1/FFOS1.0  ([firmware for ZTE open](http://www.ztedevice.com/support/smart_phone/b5a2981a-1714-4ac7-89e1-630e93e220f8.html) )
 
 ### Info
 
 * find all old, current and upcoming FirefoxOS devices on [firefoxosdevices.org](https://firefoxosdevices.org)
 * there is also an npm [cli tool to manage FirefoxOS builds](https://www.npmjs.com/package/fxosbuilds)
+
+## CouchDB setup (if you want to sync your data to your [CouchDB](https://couchdb.apache.org))
+
+you need to enable [CORS](https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+
+[github.com/pouchdb/add-cors-to-couchdb](https://github.com/pouchdb/add-cors-to-couchdb#what-it-does)
+
+*CouchDB doesn't come with CORS enabled by default. This is a problem for libraries like PouchDB, which depend on being able to access CouchDB ...*
--- app/js/app.js
+++ app/js/app.js
@@ -1,81 +1,167 @@
 'use strict';
 
-var d = document;
+var d           = document,
+    w           = window,
+    urlField    = d.getElementById('url'),
+    syncBtn     = d.getElementById('sync-btn'),
+    contactList = d.getElementById('contact-list'),
+    li          = d.createElement('LI'),
+    dbName      = 'ffos_contacts',
+    url         = 'http://10.0.0.4:5984/ffos_contacts',
+    db          = new PouchDB(dbName);
 
-if(window.navigator.mozContacts)
-{
+// check if this app has been executed on firefox os
+// else show error text in ui
+if(w.navigator.mozContacts) {
 
-  // add global for debugging on FF console
-  var accountsArr = [],
-      contactList = d.getElementById('contact-list'),
-      li          = d.createElement('LI'),
-      ipField     = d.getElementById('ip'),
-      ip          = '127.0.0.1:5984';
+    // mozObject is not a standart javascript object
+    // https://developer.mozilla.org/en-US/docs/Mozilla/Firefox_OS/API/MozContact
+    var convertMozContactToJSON = function(r) {
+        return {
+                id: r.id,
+                published: r.published,
+                updated: r.updated,
+                name: r.name,
+                honorificPrefix: r.honorificPrefix,
+                givenName: r.givenName,
+                additionalName: r.additionalName,
+                familyName: r.familyName,
+                honorificSuffix: r.honorificSuffix,
+                nickname: r.nickname,
+                email: r.email,
+                photo: r.photo,
+                url: r.url,
+                category: r.category,
+                adr: r.adr,
+                tel: r.tel,
+                org: r.org,
+                jobTitle: r.jobTitle,
+                bday: r.bday,
+                note: r.note,
+                impp: r.impp,
+                anniversary: r.anniversary,
+                sex: r.sex,
+                genderIdentity: r.genderIdentity,
+                key: r.key,
+            };
+    }
 
-  window.addEventListener('DOMContentLoaded', function() {
+    w.addEventListener('DOMContentLoaded', function() {
 
-    /*
-     * https://developer.mozilla.org/en-US/docs/Web/API/mozContact
-     */
-    var request = window.navigator.mozContacts.getAll({sortBy: 'givenName', sortOrder: 'descending'}),
-        count = 0; 
-    
-    request.onsuccess = function () {
-      if(this.result) {
-        count++;
+        //https://developer.mozilla.org/en-US/docs/Web/API/mozContact
+        var request = w.navigator.mozContacts.getAll({sortBy: 'givenName', sortOrder: 'descending'}),
+            count = 0; 
+        
+        request.onsuccess = function () {
+            if(this.result) {
+                count++;
 
-        // Display the name of the contact
-        // console.log(this.result.name[0], this.result.tel[0].value);
-        var liClone = li.cloneNode(true);
-        liClone.innerHTML = this.result.name[0];
-        contactList.appendChild(liClone);
+                // display the name of the contact
+                var liClone = li.cloneNode(true);
+                liClone.innerHTML = this.result.name[0];
+                liClone.id = this.result.id;
+                contactList.appendChild(liClone);
 
-        // push account to array
-        accountsArr.push(this.result);
+                // check if current contact is still in db
+                // and mark the contact
+                // https://pouchdb.com/api.html#fetch_document
+                db.get(this.result.id).then(function (doc) {
+                    d.getElementById(doc._id).classList.add('synced');
+                }).catch(function (err) {
+                    console.error(err);
+                });
 
-        // Move to the next contact which will call the request.onsuccess with a new result
-        this.continue();
+                // Move to the next contact which will call the request.onsuccess with a new result
+                this.continue();
 
-      } else {
-        d.getElementById('contact-count').innerHTML = count + ' contacts found';
-      }
-    };
+            } else {
+                d.getElementById('contact-count').innerHTML = count;
+            }
+        };
 
-    request.onerror = function () {
-      console.log('Could not read contact!');
-    };
+        request.onerror = function () {
+            console.error('Could not read contact!');
+        };
 
-    // get ip address while user is typing and check if format is valid
-    ipField.addEventListener('keyup',function(e)
-    {
-      if(ipField.checkValidity())
-      {
-        console.log(e.target.value);
-        ip = e.target.value;
-      }
-    });  
-    
-    d.getElementById('export').addEventListener('click',function()
-    {
 
-      for (var i = accountsArr.length - 1; i >= 0; i--) {     
-        console.log('sync: ' + accountsArr[i].name[0]);
-        fetch( 'http:' + ip + '/moz_contacts', {
-          method: 'post',
-          headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(accountsArr[i])
+
+        // get url while user is typing and check if format is valid
+        urlField.addEventListener('keyup',function(e) {
+            if(urlField.checkValidity()) {
+                url = e.target.value;
+            }
+        });    
+        
+        // write all contact to pouchdb and sync to couchdb when clicking the sync button
+        syncBtn.addEventListener('click', function() {
+            console.log('click sync btn');
+
+            var remoteDB = new PouchDB(url);
+
+            console.log('create remote db');
+
+            // https://github.com/pouchdb/pouchdb/issues/4256
+            // https://pouchdb.com/guides/replication.html
+            db.sync(remoteDB).on('complete', function () {
+                console.log('sync to CouchDB complete');
+                db.allDocs({include_docs: true, descending: true}, function(err, docs) {
+                    console.log('show local pouchdb:', docs);
+
+                    docs.rows.map(function(row) {
+                        console.log(row.doc);
+                    });
+
+                });
+            }).on('error', function (err) {
+                console.log('can not sync to CouchDB', err);
+            });
+
+            //https://developer.mozilla.org/en-US/docs/Mozilla/Firefox_OS/API/Contacts_API
+            //https://developer.mozilla.org/en-US/docs/Mozilla/Firefox_OS/API/ContactManager/find
+
+            var allContactsRequest = w.navigator.mozContacts.getAll({sortBy: 'givenName', sortOrder: 'descending'}); 
+        
+            allContactsRequest.onsuccess = function () {
+                // if one contact has been found
+                if(this.result) {
+                    // set pouchdb doc id to same id as mocContact has
+                    // and push contact to pouchdb
+
+                    var obj = {
+                        '_id': this.result.id,
+                        'doc': convertMozContactToJSON(this.result)
+                    }
+                    console.log(obj);
+                    // https://pouchdb.com/api.html#create_document
+                    db.put(obj, function callback(err, doc) {
+                        if (!err) {
+                            console.log('Successfully saved a contact!', doc);
+                            d.getElementById(doc._id).classList.add('synced');
+                        } else if (err.status === 409) {
+                            // https://pouchdb.com/guides/documents.html#updating-documents%E2%80%93correctly
+                            // https://pouchdb.com/guides/conflicts.html
+                            console.error('error status:', err.status);
+                            db.get(obj._id).then(function (doc) {
+                                //console.log('get: ', doc);
+                                var mozContact = convertMozContactToJSON(obj.doc);
+                                mozContact._id = doc._id;
+                                mozContact._rev = doc._rev;
+                                return db.put(doc);
+                            }).then(function (doc) {
+                                console.log('updated document: ',doc);
+                            });
+                        }
+                    });
+
+                    // Move to the next contact which will call the request.onsuccess with a new result
+                    this.continue();
+                }
+            };
         });
-      };
-    });
-  });
-}
-else
-{
-  d.getElementById('app-description').innerHTML = 'NOTE: you are not running FirefoxOS ! ';
-  d.getElementById('app-description').classList.add('blink');
-  d.getElementById('ip').disabled = true;
-}
 
+    });
+} else {
+    d.getElementById('app-description').innerHTML = 'NOTE: you are not running FirefoxOS ! ';
+    d.getElementById('app-description').classList.add('blink');
+    d.getElementById('url').disabled = true;
+}
